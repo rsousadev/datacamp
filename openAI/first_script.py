ee76ad6aa40110ec84cf06a26da51397f3678236
ricardo.dmsousa <ricardo.dmsousa@gmail.com>
2025-08-08 22:41:04 +0000

    feat: calculo de custo e descrição de produto

--- openAI/first_script.py
+++ openAI/first_script.py
@@ -1,14 +1,20 @@
-# Create the OpenAI client
-from openai import OpenAI
-import os
-client = OpenAI(api_key=os.getenv("OPEN_API_TOKEN"))
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
 
-# Create a request to the Chat Completions endpoint
+# Create a detailed prompt
+prompt = """
+Crie a descrição de um produto chamado SonicPro que é um fone de ouvido com
+cancelamento de ruído ativo chamado (ANC), batria que dura até 40 horas e
+tem um design elegante e drobável
+"""
+
 response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "user", 
-     "content": "Write a polite reply accepting an AI Engineer job offer."}]
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "Responda sempre em português do Brasil."},
+        {"role": "user", "content": prompt}],
+    # Experiment with max_completion_tokens and temperature settings
+    max_completion_tokens=30,
+    temperature=0
 )
 
 print(response.choices[0].message.content)
